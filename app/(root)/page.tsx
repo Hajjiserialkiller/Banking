cb103fdf2e6db708f23dd286942769dfeca693ab
ahmed hajji <125455955+Hajjiserialkiller@users.noreply.github.com>
2024-05-17 02:50:09 +0100

    Implement the sidebars

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,24 @@
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
+import RightSidebar from '@/components/RightSidebar'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Adrian', lastName: 'JSM', email: 'contact@jsmastery.pro' }
+
   return (
-    <div>
-      Home
-    </div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox type='greeting' title='Welcome' user={loggedIn?.firstName || 'Guest'} subtext='Access and manage your account and transactions efficiently.'/>
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.36} />
+        </header>
+
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{ currentBalance: 100 }, { currentBalance: 200 }]} />
+    </section>
   )
 }
 
