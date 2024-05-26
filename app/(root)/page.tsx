9046ccefb588863f8088a32feb8efdf05dc7ce19
ahmed hajji <125455955+Hajjiserialkiller@users.noreply.github.com>
2024-05-26 08:38:10 +0100

    before final commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -8,11 +8,21 @@ const Home = async () => {
   const loggedIn = await getLoggedInUser()
 
   return (
-    <section className='home'>
-      <div className='home-content'>
-        <header className='home-header'>
-          <HeaderBox type='greeting' title='Welcome' user={loggedIn?.name || 'Guest'} subtext='Access and manage your account and transactions efficiently.'/>
-          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.36} />
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox 
+            accounts={accountsData}
+            totalBanks={accounts?.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
+          />
         </header>
 
         RECENT TRANSACTIONS
